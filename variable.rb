e95abce734911fbfb30aba92384c2117f94ccefb
Tareq Sifat <41277226+tareqsifat@users.noreply.github.com>
2023-04-24 09:43:53 +0600

    variable in ruby

---
+++ variable.rb
@@ -0,0 +1,9 @@
+# there are some several types of variable in ruby
+
+# local Variable
+# Instance variable
+# Class variable
+# Global variable
+  #can declared without any sign
+  # like x = 10
+#
