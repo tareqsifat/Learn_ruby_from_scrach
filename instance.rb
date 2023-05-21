ef8675263f2f436e4c6274224dca4a3ba3989447
Tareq Sifat <41277226+tareqsifat@users.noreply.github.com>
2023-05-21 12:21:03 +0600

    Update instance learning

--- instance.rb
+++ instance.rb
@@ -19,3 +19,23 @@ $company.addEmployee
 $company.addEmployee
 
 $company.showEmployee  #should return 2
+
+
+# more about Instance for Ruby
+class Dog
+    def set_name( aName )
+        @myname = aName
+    end
+    def get_name
+        return @myname
+    end
+    def talk
+        return 'woof!'
+    end
+end
+#Now, you can create a dog, name it, display its name, and ask it to talk: 
+mydog = Dog.new 
+mydog.set_name( 'Fido' ) 
+puts(mydog.get_name) 
+puts(mydog.talk) 
+
